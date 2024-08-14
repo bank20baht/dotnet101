6f0fd3c7c57984d55c82df9d70b25c0d9b8f0051
Nattapong Promthong <banker.z412@gmail.com>
2024-08-14 20:42:28 +0700

    feat: add todo table but not finish

--- src/modules/user/models/User.cs
+++ src/modules/user/models/User.cs
@@ -1,16 +1,21 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace CleanDotnet.Model;
-
-public class User
+namespace CleanDotnet.Model
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid Id { get; set; }
 
-    [StringLength(200)]
-    public string Name { get; set; }
+        [StringLength(200)]
+        public string Name { get; set; }
 
-
+        public User()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
 }
